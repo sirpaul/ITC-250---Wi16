0111ff248d4f11b711bc1a6e7ab6ae0b02a44a12
Paul Mansoor <paul.mansoor@gmail.com>
2016-01-27 22:41:08 -0800

    Move to Objects
    New objects are created based on the user's submitted data, with the
    totals being carried within each object.

--- Item.php
+++ Item.php
@@ -3,16 +3,21 @@
 
 class Item{
     public $name = '';
-    public $description = '';
+    //public $description = '';
     public $price = 0;
+	public $quantity = 0;
+	public $total = 0;
     
-    public function __construct($name, $description, $price)
+    public function __construct($name, $quantity, $price)
     {
         $this->name = $name;
-        $this->description = $description;
         $this->price = $price;
+		$this->quantity = $quantity;
+		$this->total = $quantity * $price;
     }
+	
+	public function print_me(){
+		return "hey!";
+	}
     
 }
-
-
--- foodtruck.php
+++ foodtruck.php
@@ -5,46 +5,28 @@ define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
 
 include 'Item.php';
 
-
-// create menu items
-$pizza = new Item("Pizza","Delicious slices with your choice of toppings",5.50);
-$salad = new Item("Salad","Romaine lettuce with cheese, tomato, and crutons",6.00);
-$brownie = new Item("Brownie","Gooey and chewy chocolate",2.00);
-
-
-
-//when menu items are selected in forms, those items are added to the cart
-
-
-
-
 if(isset($_POST['submit']))
 {//data submitted
     echo '<pre>';
     var_dump($_POST);
     echo '</pre>';
-    
-    $numSlices = $_POST['numSlices'];
-    $brownies = $_POST['brownies'];
-    $salads = $_POST['salads'];
-    //$toppings = $_POST['topping'];
-    $toppings = '';
-        
-    //IF $_POST
-        //for all the stuff in POST
-        //loop through, adding each each 
-        //for($i ; $i < length($_POST; $i++){
-          //  $cart[] = new item($_POST[$i]}
-    
-    $total = ($numSlices * $pizza->price);
-    $total += ($brownies * $brownie->price);
-    $total += ($salads * $salad->price);
-    
-    if($_POST['pepperoni'] != "1"){
-           $toppings += 'pepperoni';
-    }
-    
-    echo 'The total for the order is $' .  $total;
+
+    $toppings = $_POST['toppings'];
+	$brownies = (int)$_POST['brownies'];
+	$salads = (int)$_POST['salads'];
+	$slices = (int)$_POST['numSlices'];
+		
+	//create new objects based on data entered
+	$pizza = new Item("Pizza", $slices, 5.50);
+	$salad = new Item("Salad", $salads, 6.00);
+	$brownie = new Item("Brownie", $brownies, 2.00);
+	$toppingsPrice = 0.50;
+	$toppingsTotal = count($toppings) * $toppingsPrice * $slices;
+	$total = $pizza->total + $salad->total + $brownie->total +$toppingsTotal;
+		
+	echo 'You ordered ' . $slices . ' slices of pizza with '. $salads . ' salads and ' . $brownies . ' brownie(s).';
+	echo '<br/>';
+	echo 'The total for the order is $' .  $total . ', thank you for your purchase!';
 }else{//show form
     echo '
     <form method="post" action="' . THIS_PAGE . '">
@@ -52,19 +34,19 @@ if(isset($_POST['submit']))
     <legend>Pizza</legend>
     
     Select toppings:<br />
-    <input type="checkbox" value="1" name="pepperoni">Pepperoni<br/>
-    <input type="checkbox" value="salami" name="topping">Salami<br/>
-    <input type="checkbox" value="pineapple" name="topping">Pineapple<br/>
-    <input type="checkbox" value="sausage" name="topping">Sausage<br/>
-    <input type="checkbox" value="mushroom" name="topping">Mushroom<br/>
+    <input type="checkbox" value="pepperoni" name="toppings[]">Pepperoni<br/>
+    <input type="checkbox" value="salami" name="toppings[]">Salami<br/>
+    <input type="checkbox" value="pineapple" name="toppings[]">Pineapple<br/>
+    <input type="checkbox" value="sausage" name="toppings[]">Sausage<br/>
+    <input type="checkbox" value="mushroom" name="toppings[]">Mushroom<br/>
     </select><br />
     
     How many slices?<br />
     <select name="numSlices">
-    <option value="0">zero</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
+    <option name="slices[]" value="0">zero</option>
+    <option name="slices[]" value="1">One</option>
+    <option name="slices[]" value="2">Two</option>
+    <option name="slices[]" value="3">Three</option>
     </select><br /><br />
     </fieldset>
     
